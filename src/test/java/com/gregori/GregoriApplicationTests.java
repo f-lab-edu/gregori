71cde2744688b71b0d7494f56cdf966833de714f
jiunkoo <jiunkoo0106@gmail.com>
2023-10-26 20:50:59 +0900

    gregori 프로젝트 추가

---
+++ src/test/java/com/gregori/GregoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gregori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GregoriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
