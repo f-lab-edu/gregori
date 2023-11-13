5564143991cc6d2adaed24154912c01b966c5c03
jiunkoo <jiunkoo0106@gmail.com>
2023-11-13 20:34:45 +0900

    #2 member domain 추가

---
+++ src/main/java/com/gregori/domain/member/Member.java
@@ -0,0 +1,34 @@
+package com.gregori.domain.member;
+
+import com.gregori.domain.AbstractEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
+
+@Getter
+public class Member extends AbstractEntity {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Status status;
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Status {
+        ACTIVATE("활성화"), DEACTIVATE("비활성화");
+        private final String description;
+    }
+
+    public Member(String name, String email, String password) {
+        if (!StringUtils.hasText(name)) throw new RuntimeException("Empty name");
+        if (!StringUtils.hasText(email)) throw new RuntimeException("Empty email");
+        if (!StringUtils.hasText(password)) throw new RuntimeException("Empty password");
+
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.status = Status.ACTIVATE;
+    }
+}
