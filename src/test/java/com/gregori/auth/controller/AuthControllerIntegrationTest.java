3da9dfc85305362358d2eb5385b893a9ea94058d
jiunkoo <jiunkoo0106@gmail.com>
2023-12-13 20:31:06 +0900

    #17 빌드 버그 수정 - 어노테이션 변경

--- src/test/java/com/gregori/auth/controller/AuthControllerIntegrationTest.java
+++ src/test/java/com/gregori/auth/controller/AuthControllerIntegrationTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
@@ -40,8 +41,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
+@WebMvcTest(value = AuthController.class)
 class AuthControllerIntegrationTest {
 	@Autowired
 	private MockMvc mockMvc;
