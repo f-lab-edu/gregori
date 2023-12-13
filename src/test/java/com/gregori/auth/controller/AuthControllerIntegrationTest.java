765fadc86c71a3abb9e680fd8c54fc6fc3442f6f
jiunkoo <jiunkoo0106@gmail.com>
2023-12-13 20:50:36 +0900

    #17 빌드 버그 수정 - 어노테이션 변경

--- src/test/java/com/gregori/auth/controller/AuthControllerIntegrationTest.java
+++ src/test/java/com/gregori/auth/controller/AuthControllerIntegrationTest.java
@@ -41,7 +41,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @ActiveProfiles("test")
-@WebMvcTest(value = AuthController.class)
+@AutoConfigureMockMvc
 class AuthControllerIntegrationTest {
 	@Autowired
 	private MockMvc mockMvc;
