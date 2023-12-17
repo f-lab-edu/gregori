935077ddc98979f0cd4d82c9cb460e1ed6066a50
jiunkoo <jiunkoo0106@gmail.com>
2023-12-17 18:16:11 +0900

    #17 refactor: refresh token 테스트 추가

---
+++ src/test/java/com/gregori/refresh_token/domain/RefreshTokenTest.java
@@ -0,0 +1,28 @@
+package com.gregori.refresh_token.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RefreshTokenTest {
+
+	@Test
+	void updateRefreshTokenValue() {
+	}
+
+	@Test
+	void getId() {
+	}
+
+	@Test
+	void getRefreshTokenKey() {
+	}
+
+	@Test
+	void getRefreshTokenValue() {
+	}
+
+	@Test
+	void builder() {
+	}
+}
