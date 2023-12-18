181ceaa56253f4c03baaaab55ab51544689b627f
jiunkoo <jiunkoo0106@gmail.com>
2023-12-18 21:02:32 +0900

    #17 test: refreshToken 도메인 테스트 추가

--- src/test/java/com/gregori/refresh_token/domain/RefreshTokenTest.java
+++ src/test/java/com/gregori/refresh_token/domain/RefreshTokenTest.java
@@ -1,28 +1,40 @@
 package com.gregori.refresh_token.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class RefreshTokenTest {
 
 	@Test
-	void updateRefreshTokenValue() {
+	@DisplayName("RefreshToken 객체의 필드를 수정한다.")
+	void should_updateRefreshTokenValue() {
+		// given
+		RefreshToken refreshToken = new RefreshToken("key", "value");
+
+		// when
+		refreshToken.updateRefreshTokenValue("new value");
+
+		// then
+		assertEquals(refreshToken.getRefreshTokenValue(), "new value");
 	}
 
 	@Test
-	void getId() {
-	}
+	@DisplayName("Member 객체의 필드를 builder 패턴으로 생성하고 getter 메서드로 조회한다.")
+	void should_getFields_when_buildMember() {
+		// given
+		RefreshToken refreshToken = RefreshToken.builder()
+			.refreshTokenKey("key")
+			.refreshTokenValue("value")
+			.build();
 
-	@Test
-	void getRefreshTokenKey() {
-	}
-
-	@Test
-	void getRefreshTokenValue() {
-	}
-
-	@Test
-	void builder() {
+		// when, then
+		assertNull(refreshToken.getId());
+		assertEquals(refreshToken.getRefreshTokenKey(), "key");
+		assertEquals(refreshToken.getRefreshTokenValue(), "value");
+		assertNull(refreshToken.getCreatedAt());
+		assertNull(refreshToken.getUpdatedAt());
 	}
 }
