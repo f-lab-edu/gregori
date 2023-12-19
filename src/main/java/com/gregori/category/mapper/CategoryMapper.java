57fdd3a73dada13be09567267ca6183037223084
jiunkoo <jiunkoo0106@gmail.com>
2023-12-19 20:34:13 +0900

    #16 feat: category mapper 추가

---
+++ src/main/java/com/gregori/category/mapper/CategoryMapper.java
@@ -0,0 +1,18 @@
+package com.gregori.category.mapper;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gregori.category.domain.Category;
+
+@Mapper
+public interface CategoryMapper {
+
+	Long insert(Category category);
+	Long updateName(Category category);
+	Long deleteById(Long categoryId);
+	Optional<Category> findById(Long categoryId);
+	List<Category> find();
+}
